48218f21314c25af11ac744fbe736e2053da0c0d
Shamshad Ahmad <shamshad.ahmad@stits.co>
2021-05-19 16:54:15 +0530

    commit  demo

---
+++ Model/DBTESTContext.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace TestApplication.Model
+{
+    public partial class DBTESTContext : DbContext
+    {
+        public DBTESTContext()
+        {
+        }
+
+        public DBTESTContext(DbContextOptions<DBTESTContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-POJ0U4S\\;Database=DBTEST;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Latin1_General_CI_AS");
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.ToTable("students");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Address)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("address");
+
+                entity.Property(e => e.Code)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("code");
+
+                entity.Property(e => e.Contactno)
+                    .HasMaxLength(30)
+                    .IsUnicode(false)
+                    .HasColumnName("contactno");
+
+                entity.Property(e => e.Emailid)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("emailid");
+
+                entity.Property(e => e.Firstname)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("firstname");
+
+                entity.Property(e => e.Lastname)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("lastname");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
